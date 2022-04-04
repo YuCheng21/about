7df76243484a023d2469ee85ab8665a6203f39b9
YuCheng <fyc621@gmail.com>
2022-04-04 11:17:54 +0800

    add: some layout #20220404002

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
+        cssnano: {
+            preset: 'default',
+        },
     },
     sourceMap: true,
 };
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,2 +1,6 @@
 import "../css/main.css";
 import '@iconify/iconify';
+
+import zh from '../data/zh.json'
+
+console.log(zh)
