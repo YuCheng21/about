310ab22f578097611f465820963943e042000ea4
YuCheng <fyc621@gmail.com>
2023-02-27 21:31:51 +0800

    fix: print result #20230227001

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./resources/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        'sm': {'raw': 'print, (min-width: 640px)'},
+      },
       colors: {
         'primary': colors.blue,
       }
